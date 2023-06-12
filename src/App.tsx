51026f1fe8ac5e15f4b082f851e64695397f1666
Liam Ansoumane Kadher Kande <liamkande@icloud.com>
2023-06-11 18:59:01 -0700

    added logic for deposit and cashback

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
-function App() {
+const App: React.FC = () => {
+  const [deposit, setDeposit] = useState(0)
+  const [totalState, setTotalState] = useState(0)
+  const [isDeposit, setIsDeposit] = useState(true)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDeposit(Number(event.target.value))
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    if (isDeposit) {
+      setTotalState(totalState + deposit)
+    } else {
+      if (deposit > totalState) {
+        alert("Insufficient funds")
+      } else {
+        setTotalState(totalState - deposit)
+      }
+    }
+
+    setDeposit(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form className="container" onSubmit={handleSubmit}>
+      <h2 id="total">Account Balance $ {totalState}</h2>
+      <div className="select-container">
+        <label>Select an action below to continue</label>
+        <select
+          onChange={(e) => setIsDeposit(e.target.value === "Deposit")}
+          className="select-field"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <option id="no-selection" value="" />
+          <option id="deposit-selection" value="Deposit">
+            Deposit
+          </option>
+          <option id="cashback-selection" value="Cash Back">
+            Cash Back
+          </option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="number-input">Amount:</label>
+        <input
+          id="number-input"
+          type="number"
+          className="input-field"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="submit"
+          className="button"
+          value="Submit"
+          id="submit-input"
+        />
+      </div>
+    </form>
+  )
 }
 
-export default App;
+export default App
